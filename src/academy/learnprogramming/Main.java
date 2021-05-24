175570369d19ba2e6496e920e9abc0ac84fd8fd2
Preeti Rawat <preetirawat711@gmail.com>
2021-05-24 18:28:12 +0530

    Challenge2 some modifications.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,7 +3,7 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
- Theatre theatre= new Theatre("Olumpian", 8,12);
+ /*Theatre theatre= new Theatre("Olumpian", 8,12);
     //theatre.getSeats();
     if(theatre.reserveSeat("H11")){
         System.out.println("Please Pay");
@@ -15,6 +15,6 @@ public class Main {
         }else{
             System.out.println("Sorry, seat is taken");
         }
-
+*/
     }
 }
