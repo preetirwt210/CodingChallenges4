fad4b7a07be945413dc808de76ad353baa4031c5
Preeti Rawat <preetirawat711@gmail.com>
2021-05-28 17:55:40 +0530

    Properties class

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,8 +1,13 @@
 package academy.learnprogramming;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
  /*Theatre theatre= new Theatre("Olumpian", 8,12);
     //theatre.getSeats();
     if(theatre.reserveSeat("H11")){
@@ -16,5 +21,13 @@ public class Main {
             System.out.println("Sorry, seat is taken");
         }
 */
+        Properties p = System.getProperties();
+        Set set = p.entrySet();
+
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
     }
 }
