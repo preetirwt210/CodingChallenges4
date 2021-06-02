f8d011946a64011b65787830d8968366f4a9b1c9
Preeti Rawat <preetirawat711@gmail.com>
2021-06-02 18:23:19 +0530

    challenge1

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,20 +1,16 @@
 package academy.learnprogramming;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
- Theatre theatre= new Theatre("Olumpian", 8,12);
-    //theatre.getSeats();
-    if(theatre.reserveSeat("D12")){
-        System.out.println("Please Pay");
-    }else{
-        System.out.println("Sorry, seat is taken");
-    }
+      /*  Theatre theatre= new Theatre("Olumpian", 8,12);
+        //theatre.getSeats();
+        if(theatre.reserveSeat("D12")){
+            System.out.println("Please Pay");
+        }else{
+            System.out.println("Sorry, seat is taken");
+        }
 //        if(theatre.reserveSeat("H11")){
 //            System.out.println("Please Pay");
 //        }else{
@@ -29,5 +25,9 @@ public class Main {
             Map.Entry entry = (Map.Entry) itr.next();
             System.out.println(entry.getKey() + " = " + entry.getValue());
         }*/
+        Main2 main= new Main2();
+        main.command();
     }
+
+
 }
