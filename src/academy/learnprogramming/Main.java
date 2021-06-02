636ebcb9f2e9f62cef485284704ff9fef30c7e63
Preeti Rawat <preetirawat711@gmail.com>
2021-06-02 17:37:25 +0530

    binary search

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -8,26 +8,26 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) throws Exception {
- /*Theatre theatre= new Theatre("Olumpian", 8,12);
+ Theatre theatre= new Theatre("Olumpian", 8,12);
     //theatre.getSeats();
-    if(theatre.reserveSeat("H11")){
+    if(theatre.reserveSeat("D12")){
         System.out.println("Please Pay");
     }else{
         System.out.println("Sorry, seat is taken");
     }
-        if(theatre.reserveSeat("H11")){
-            System.out.println("Please Pay");
-        }else{
-            System.out.println("Sorry, seat is taken");
-        }
-*/
-        Properties p = System.getProperties();
+//        if(theatre.reserveSeat("H11")){
+//            System.out.println("Please Pay");
+//        }else{
+//            System.out.println("Sorry, seat is taken");
+//        }
+
+   /*     Properties p = System.getProperties();
         Set set = p.entrySet();
 
         Iterator itr = set.iterator();
         while (itr.hasNext()) {
             Map.Entry entry = (Map.Entry) itr.next();
             System.out.println(entry.getKey() + " = " + entry.getValue());
-        }
+        }*/
     }
 }
--- src/academy/learnprogramming/Theatre.java
+++ src/academy/learnprogramming/Theatre.java
@@ -20,26 +20,25 @@ public class Theatre {
         return threatreName;
     }
     public boolean reserveSeat(String seatNumber){
-        Seat requestedSeat=new Seat(seatNumber);
-        int foundSeat=Collections.binarySearch(seats,requestedSeat,null);
-            if(foundSeat>=0){
-                return seats.get(foundSeat).reserve();
-            }else{
-                System.out.println("There is no seat "+ seatNumber);
-                return false;
-            }
-   /*     for(Seat seat : seats){
-            if(seat.getSeatNumber().equals(seatNumber)){
-            requestedSeat=seat;
-             break;
-            }
+        int low = 0;
+        int high = seats.size()-1;
 
-       }
-        if(requestedSeat==null){
-            System.out.println("There is no seat"+ seatNumber);
-             return false;
+        while (low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return seats.get(mid).reserve();
+            }
         }
-        return requestedSeat.reserve();*/
+        System.out.println("There is no seat " + seatNumber);
+        return false;
     }
     public void getSeats(){
         for(Seat seat: seats){
